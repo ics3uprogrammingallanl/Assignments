b6efde48fa104035132fde1b377a577168f5ca68
AL10120 <allan.leveille@ocsbstudent.ca>
2019-03-04 12:31:23 -0500

    Initial commit

---
+++ FallingObjectsAllanL/FallingObjectsAllanL/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FallingObjectsAllanL
+{
+    partial class frmFallingObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(552, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(243, 96);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 1;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(51, 50);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(465, 20);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "Enter the time (in seconds) since you released the object";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(23, 266);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(363, 20);
+            this.lblHeight.TabIndex = 3;
+            this.lblHeight.Text = "The height of the object above the ground is";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(219, 153);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(146, 59);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(436, 271);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 20);
+            this.lblAnswer.TabIndex = 5;
+            // 
+            // frmFallingObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FallingObjectsAllanL.Properties.Resources.math_talks;
+            this.ClientSize = new System.Drawing.Size(552, 357);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmFallingObjects";
+            this.Text = "Falling Objects by AllanL";
+            this.Load += new System.EventHandler(this.frmFallingObjects_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ FallingObjectsAllanL/FallingObjectsAllanL/Form1.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by: Allan L
+ * Created on: February 19 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #10 - Name of Program
+ * This program calculates the amount of time it takes for an object hit the ground.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjectsAllanL
+{
+    public partial class frmFallingObjects : Form
+    {
+        public frmFallingObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            String userIn = txtTime.Text;
+
+            if (0 == 1)
+            {
+                lblAnswer.Text = "Input a number!";
+            }
+            else
+            {
+                lblAnswer.Text = Convert.ToString(100 - 0.5 * 9.81 * Math.Pow(Double.Parse(userIn), 2));
+            }
+
+        }
+
+        private void frmFallingObjects_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
